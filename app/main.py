d6f583bbe578839d1461618a65ccacf44457d1f9
Shristi-iitm <shristi7897@gmail.com>
2025-07-25 16:32:20 +0000

    feat: update welcome message for demo

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class IrisInput(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Fully Automated Iris Prediction API."}
+    return {"message": "Welcome to the Fully Automated Iris Prediction API using Docker and Kubernetes."}
 
 @app.post("/predict")
 def predict_species(iris_input: IrisInput):
